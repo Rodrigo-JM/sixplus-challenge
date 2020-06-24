f91c80ca13ce1b8444772502e3e67abbedcda6d8
rodrigojm <rodrigomallmann1@gmail.com>
2020-06-24 11:47:43 -0400

    added fonts and iconds

--- client/components/ShipmentCard.js
+++ client/components/ShipmentCard.js
@@ -1,13 +1,15 @@
 import React from 'react'
-
+import RoomSharpIcon from '@material-ui/icons/RoomSharp';
+import RedoSharpIcon from '@material-ui/icons/RedoSharp';
 export default function ShipmentCard(props) {
+
     return (
         <div onClick={() => props.goToShipment(props.data.id)} className="card">
             <div className="basic-info">
+                <h3>{props.data.name}</h3>
+                <h3><RoomSharpIcon fontSize="default"/>   {props.data.origin}</h3>
+                <h3><RedoSharpIcon fontSize="default"/>   {props.data.destination}</h3>
                 <h3>ID: {props.data.id}</h3>
-                <h3>Name: {props.data.name}</h3>
-                <h3>Origin: {props.data.origin}</h3>
-                <h3>Destination: {props.data.destination}</h3>
             </div>
             <div className={`status-color-tag-${props.data.status.toLowerCase()}`}>
             </div>
